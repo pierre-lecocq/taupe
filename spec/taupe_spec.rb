ff6b794ee6c4ad176dbc9acca53388c77f2115b6
Pierre Lecocq <pierre.lecocq@gmail.com>
2014-12-10 15:59:30 +0100

    gem v0.6.3

--- spec/taupe_spec.rb
+++ spec/taupe_spec.rb
@@ -1,5 +1,5 @@
 # File: taupe_spec.rb
-# Time-stamp: <2014-12-10 15:42:44 pierre>
+# Time-stamp: <2014-12-10 15:59:17 pierre>
 # Copyright (C) 2014 Pierre Lecocq
 # Description: Taupe library main tests file
 
@@ -7,8 +7,8 @@ require_relative '../lib/taupe'
 
 describe Taupe do
   describe 'VERSION' do
-    it 'should return 0.6.2' do
-      expect(Taupe::VERSION).to eql '0.6.2'
+    it 'should return 0.6.3' do
+      expect(Taupe::VERSION).to eql '0.6.3'
     end
   end
 end
